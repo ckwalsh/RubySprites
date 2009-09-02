f209c6e344168b8a4fdc8aae5e72b031bd804319
Cullen Walsh <ckwalsh@ckwalsh-netbook.(none)>
2009-09-01 21:54:47 -0700

    Creating examples directory, adding css generator logic

---
+++ examples/css/css.rb
@@ -0,0 +1,17 @@
+current_dir = File.dirname(__FILE__)
+
+$:.unshift File.join(current_dir, '../../lib')
+
+require 'ruby_sprites/sprite'
+require 'ruby_sprites/css'
+
+sprite = RubySprites::Sprite.new('css/sprite.png', current_dir + '/..')
+    
+(1..60).each do |x|
+  sprite.add_image("example_imgs/#{x}.png")
+end
+
+sprite.update
+
+puts RubySprites::CSS.generate(sprite)
+RubySprites::CSS.generate(sprite, 'css/css.css')
