8f42c53fd6da5644ad71bb4673b7bf449514963e
Cullen Walsh <ckwalsh@ckwalsh-desktop.(none)>
2009-09-14 13:05:24 -0700

    Oops, forgot it needed to be negative

--- lib/ruby_sprites/css.rb
+++ lib/ruby_sprites/css.rb
@@ -12,7 +12,7 @@ module RubySprites
 }\n\n"
       sprite.images.each do |path, image|
         class_name = path[0,path.rindex('.')].gsub('/','_')
-        css += sprintf(css_template, class_name, sprite.filename, image.x, image.y, image.height, image.width)
+        css += sprintf(css_template, class_name, sprite.filename, -image.x, -image.y, image.height, image.width)
       end
 
       if file.nil?
